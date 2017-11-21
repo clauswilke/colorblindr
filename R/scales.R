349905590cfad44bcfc58d3909d5e91f3acf5794
Claus Wilke <wilke@austin.utexas.edu>
2017-11-20 22:37:29 -0600

    Add ability to lighten or darken Okabe Ito color scale.

--- R/scales.R
+++ R/scales.R
@@ -1,8 +1,8 @@
 #' @rdname scale_OkabeIto
 #' @export
 #' @usage NULL
-scale_colour_OkabeIto <- function(...) {
-  scale_OkabeIto("colour", ...)
+scale_colour_OkabeIto <- function(aesthetics = "colour", ...) {
+  scale_OkabeIto(aesthetics, ...)
 }
 
 #' @rdname scale_OkabeIto
@@ -13,8 +13,8 @@ scale_color_OkabeIto <- scale_colour_OkabeIto
 #' @rdname scale_OkabeIto
 #' @export
 #' @usage NULL
-scale_fill_OkabeIto <- function(...) {
-  scale_OkabeIto("fill", ...)
+scale_fill_OkabeIto <- function(aesthetics = "fill", ...) {
+  scale_OkabeIto(aesthetics, ...)
 }
 
 #' Okabe-Ito color scale
@@ -22,8 +22,10 @@ scale_fill_OkabeIto <- function(...) {
 #' This is a color-blind friendly, qualitative scale with eight different colors. See [palette_OkabeIto] for details.
 #' @param use_black If `TRUE`, scale includes black, otherwise includes gray.
 #' @param order Numeric vector listing the order in which the colors should be used. Default is 1:8.
-#' @param ... common discrete scale parameters: `name`, `breaks`, `labels`, `na.value`, `limits` and `guide`. See
-#'  [discrete_scale] for more details.
+#' @param darken Relative amount by which the scale should be darkened (for positive values) or lightened (for negatice
+#'   values).
+#' @param ... common discrete scale parameters: `name`, `breaks`, `labels`, `na.value`, `limits`, `guide`, and `aesthetics`.
+#'  See [discrete_scale] for more details.
 #' @examples
 #' library(ggplot2)
 #' ggplot(iris, aes(Sepal.Length, Sepal.Width, color = Species)) +
@@ -32,13 +34,18 @@ scale_fill_OkabeIto <- function(...) {
 #'   geom_density(alpha = 0.7) + scale_fill_OkabeIto(order = c(1, 3, 5))
 #' @export
 #' @usage NULL
-scale_OkabeIto <- function(aesthetic, use_black = FALSE, order = 1:8, ...) {
+scale_OkabeIto <- function(aesthetics, use_black = FALSE, order = 1:8, darken = 0, ...) {
   if (use_black) {
     values <- palette_OkabeIto_black[order]
   }
   else {
     values <- palette_OkabeIto[order]
   }
+
+  if (darken != 0) {
+    values <- colorspace::darken(values, amount = darken)
+  }
+
   pal <- function(n) {
     if (n > length(values)) {
       warning("Insufficient values in manual scale. ", n, " needed but only ",
@@ -46,5 +53,5 @@ scale_OkabeIto <- function(aesthetic, use_black = FALSE, order = 1:8, ...) {
     }
     values
   }
-  ggplot2::discrete_scale(aesthetic, "manual", pal, ...)
+  ggplot2::discrete_scale(aesthetics, "manual", pal, ...)
 }
