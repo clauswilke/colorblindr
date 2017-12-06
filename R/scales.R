14c7f71949ca87a604d49caf5e859c6606f8bee3
Claus Wilke <wilke@austin.utexas.edu>
2017-12-06 16:57:35 -0600

    scales improvements

--- R/scales.R
+++ R/scales.R
@@ -24,6 +24,7 @@ scale_fill_OkabeIto <- function(aesthetics = "fill", ...) {
 #' @param order Numeric vector listing the order in which the colors should be used. Default is 1:8.
 #' @param darken Relative amount by which the scale should be darkened (for positive values) or lightened (for negatice
 #'   values).
+#' @param alpha Alpha transparency level of the color. Default is no transparency.
 #' @param ... common discrete scale parameters: `name`, `breaks`, `labels`, `na.value`, `limits`, `guide`, and `aesthetics`.
 #'  See [discrete_scale] for more details.
 #' @examples
@@ -32,9 +33,18 @@ scale_fill_OkabeIto <- function(aesthetics = "fill", ...) {
 #'   geom_point() + scale_color_OkabeIto()
 #' ggplot(iris, aes(Sepal.Length, fill = Species)) +
 #'   geom_density(alpha = 0.7) + scale_fill_OkabeIto(order = c(1, 3, 5))
+#'
+#' cowplot::plot_grid(
+#'   gg_color_swatches(8) + scale_fill_OkabeIto(darken = 0.6),
+#'   gg_color_swatches(8) + scale_fill_OkabeIto(darken = 0.4),
+#'   gg_color_swatches(8) + scale_fill_OkabeIto(darken = 0.2),
+#'   gg_color_swatches(8) + scale_fill_OkabeIto(darken = 0),
+#'   gg_color_swatches(8) + scale_fill_OkabeIto(darken = -0.2),
+#'   gg_color_swatches(8) + scale_fill_OkabeIto(darken = -0.4),
+#'   gg_color_swatches(8) + scale_fill_OkabeIto(darken = -0.6), ncol = 1)
 #' @export
 #' @usage NULL
-scale_OkabeIto <- function(aesthetics, use_black = FALSE, order = 1:8, darken = 0, ...) {
+scale_OkabeIto <- function(aesthetics, use_black = FALSE, order = 1:8, darken = 0, alpha = NA, ...) {
   if (use_black) {
     values <- palette_OkabeIto_black[order]
   }
@@ -42,8 +52,23 @@ scale_OkabeIto <- function(aesthetics, use_black = FALSE, order = 1:8, darken = 
     values <- palette_OkabeIto[order]
   }
 
-  if (darken != 0) {
-    values <- colorspace::darken(values, amount = darken)
+  n <- length(values)
+  darken <- rep_len(darken, n)
+  alpha <- rep_len(alpha, n)
+
+  di <- darken > 0
+  if (sum(di) > 0) { # at least one color needs darkening
+    values[di] <- colorspace::darken(values[di], amount = darken[di])
+  }
+
+  li <- darken < 0
+  if (sum(li) > 0) { # at least one color needs lightening
+    values[li] <- colorspace::lighten(values[li], amount = -1*darken[li])
+  }
+
+  ai <- !is.na(alpha)
+  if (sum(ai) > 0) { # at least one color needs alpha
+    values[ai] <- scales::alpha(values[ai], alpha[ai])
   }
 
   pal <- function(n) {
--- inst/doc/introduction.R
+++ inst/doc/introduction.R
@@ -6,38 +6,33 @@ library(colorblindr)
 p <- ggplot(iris, aes(Sepal.Width, fill=Species)) +
   geom_density(alpha = 0.7)
 
-p2 <- edit_colors(p, deutan, sev = 1)
-p3 <- edit_colors(p, protan, sev = 1)
-p4 <- edit_colors(p, tritan, sev = 1)
-plot_grid(p, p2, p3, p4, labels = c("Original", "Deutan10", "Protan10", "Tritan10"))
+cvd_grid(p)
 
 ## ------------------------------------------------------------------------
+cvd_grid(p, sev = 0.5)
 
+## ----eval=FALSE----------------------------------------------------------
+#  view_cvd(p)
 
-p <- ggplot(iris, aes(Sepal.Width, fill=Species)) +
-  geom_density(alpha = 0.7)
+## ----echo=FALSE, out.width=800-------------------------------------------
+knitr::include_graphics("shiny_screenshot_view_cvd.PNG")
 
-p2 <- edit_colors(p, deutan, sev = .5)
-p3 <- edit_colors(p, protan, sev = .5)
-p4 <- edit_colors(p, tritan, sev = .5)
-plot_grid(p, p2, p3, p4, labels = c("Original", "Deutan5", "Protan5", "Tritan5"))
-
-## ------------------------------------------------------------------------
+## ----fig.height=3--------------------------------------------------------
 p2 <- p + scale_fill_OkabeIto()
 plot_grid(p, p2)
 
+## ----fig.height=3--------------------------------------------------------
+p2 <- edit_colors(p, tritan, sev=0.8)
+plot_grid(p, p2)
 
 ## ------------------------------------------------------------------------
-
 p2 <- edit_colors(p, desaturate, amount = .3)
 p3 <- edit_colors(p, desaturate, amount = .6)
 p4 <- edit_colors(p, desaturate, amount = 1)
 
 plot_grid(p, p2, p3, p4)
 
-## ------------------------------------------------------------------------
-
-edit_colors(p, colfun = function(c) {"#111111"}, fillfun =  function(c){"#FFFFFF"})
+## ----fig.height=3--------------------------------------------------------
 
 to_white <- function(c) {"#FFFFFF"} # convert everything to white
 to_black <- function(c) {"#000000"} # convert everything to black
@@ -45,15 +40,42 @@ to_black <- function(c) {"#000000"} # convert everything to black
 p2 <- edit_colors(p, colfun = to_white, fillfun = to_black)
 p3 <- edit_colors(p, colfun = to_black, fillfun = to_white)
 
-plot_grid(p,p2,p3, nrow=1)
+plot_grid(p2,p3, nrow=1)
 
-## ------------------------------------------------------------------------
+## ----fig.height=3--------------------------------------------------------
 library(magick)
 p <- ggdraw() + draw_image("HSV-color-wheel.png") # turn png into ggplot object
-p2 <- edit_colors(p, deutan, sev = .3)
-p3 <- edit_colors(p, deutan, sev = .7)
-p4 <- edit_colors(p, deutan, sev = 1)
+p2 <- edit_colors(p, deutan, severity = .3)
+p3 <- edit_colors(p, deutan, severity = .7)
+p4 <- edit_colors(p, deutan, severity = 1)
 
 plot_grid(p, p2, p3, p4, nrow=1)
+
+## ------------------------------------------------------------------------
+
+p <- ggdraw() + draw_image("FluorescentCells.jpg") # turn jpg into ggplot object
+
+to_red <- function(c){simulate_cvd(c, matrix(c(
+    1, 0, 0,
+    0,  0,  0,
+    0,  0, 0 )
+ ,3,3,byrow=TRUE))}
+
+to_green <- function(c){simulate_cvd(c, matrix(c(
+    0, 0, 0,
+    0,  1,  0,
+    0,  0, 0 )
+ ,3,3,byrow=TRUE))} 
+
+to_blue <- function(c){simulate_cvd(c, matrix(c(
+    0, 0, 0,
+    0,  0,  0,
+    0,  0, 1 )
+ ,3,3,byrow=TRUE))} 
+
+p2 <-  edit_colors(p, to_red )
+p3 <-  edit_colors(p, to_green )
+p4 <-  edit_colors(p, to_blue )
+plot_grid(p,p2,p3,p4)
 
 
