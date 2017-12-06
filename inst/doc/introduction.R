7f8c336309d7d4119d7ae00650dc4e56470784b2
Claus Wilke <wilke@austin.utexas.edu>
2017-12-06 17:05:18 -0600

    fix build

--- inst/doc/introduction.R
+++ inst/doc/introduction.R
@@ -1,6 +1,4 @@
 ## ----basic, warning=FALSE, message=FALSE---------------------------------
-library(cowplot) # also loads ggplot2
-library(colorspace) # for desaturate
 library(colorblindr)
 
 p <- ggplot(iris, aes(Sepal.Width, fill=Species)) +
@@ -17,11 +15,28 @@ cvd_grid(p, sev = 0.5)
 ## ----echo=FALSE, out.width=800-------------------------------------------
 knitr::include_graphics("shiny_screenshot_view_cvd.PNG")
 
-## ----fig.height=3--------------------------------------------------------
-p2 <- p + scale_fill_OkabeIto()
-plot_grid(p, p2)
+## ----fig.height=3, warning = FALSE, message = FALSE----------------------
+library(cowplot) #For plot_grid
 
-## ----fig.height=3--------------------------------------------------------
+swatch <- ggplot(data= data.frame(color_id = c(1,2,3,4,5,6,7, 8)), aes(x=color_id, y=1, value=color_id, fill=as.character(color_id))) +
+      geom_tile() +
+      geom_text(aes(label=color_id), color="white", size=5) +
+      theme_nothing() +
+      theme(legend.position = "none")
+
+okabe_ito <- swatch + scale_fill_OkabeIto()
+with_black <- swatch + scale_fill_OkabeIto(use_black = TRUE) 
+darker <- swatch + scale_fill_OkabeIto(darken = 0.4)
+lighter <- swatch + scale_fill_OkabeIto(darken = -0.2)
+
+plot_grid(okabe_ito, with_black, darker, lighter, ncol = 1)
+
+## ----fig.height=3, warning = FALSE, message = FALSE----------------------
+p2 <- p + scale_fill_OkabeIto()
+p3 <- p + scale_fill_OkabeIto(order = c(6, 4, 2))
+plot_grid(p2, p3, nrow = 1)
+
+## ----fig.height = 3------------------------------------------------------
 p2 <- edit_colors(p, tritan, sev=0.8)
 plot_grid(p, p2)
 
@@ -32,7 +47,7 @@ p4 <- edit_colors(p, desaturate, amount = 1)
 
 plot_grid(p, p2, p3, p4)
 
-## ----fig.height=3--------------------------------------------------------
+## ----fig.height = 3------------------------------------------------------
 
 to_white <- function(c) {"#FFFFFF"} # convert everything to white
 to_black <- function(c) {"#000000"} # convert everything to black
@@ -42,7 +57,7 @@ p3 <- edit_colors(p, colfun = to_black, fillfun = to_white)
 
 plot_grid(p2,p3, nrow=1)
 
-## ----fig.height=3--------------------------------------------------------
+## ----fig.height = 3, message = FALSE, warning = FALSE--------------------
 library(magick)
 p <- ggdraw() + draw_image("HSV-color-wheel.png") # turn png into ggplot object
 p2 <- edit_colors(p, deutan, severity = .3)
